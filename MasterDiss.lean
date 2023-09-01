4bf38308fff37273c98b5845403a05b9fb2430e2
Hyeokjun Kwon <rnjsgurwns27@gmail.com>
2023-09-01 23:35:44 +0900

    Some progress on proving Lemma 2.27

--- MasterDiss.lean
+++ MasterDiss.lean
@@ -39,6 +39,143 @@ lemma Halfspace_convex (H_ : Halfspace d) : Convex ℝ H_.S := by
   rw [H_.h]
   exact convex_halfspace_le (LinearMap.isLinear H_.f.1) H_.α
 
+def RelativeInterior (X : Set (EuclideanSpace ℝ (Fin d))) : Set (EuclideanSpace ℝ (Fin d)) := by
+  apply (·.val) '' (@interior (affineSpan ℝ X) _ _) 
+  exact setOf (λ x => x.val ∈ X)
+
+def RelativeFrontier (X : Set (EuclideanSpace ℝ (Fin d))) : Set (EuclideanSpace ℝ (Fin d)) := by
+  apply (·.val) '' (@frontier (affineSpan ℝ X) _ _) 
+  exact setOf (λ x => x.val ∈ X)
+
+def IsFace (F X : Set (EuclideanSpace ℝ (Fin d))) : Prop := 
+  Convex ℝ F ∧ IsClosed F ∧ IsExtreme ℝ X F
+
+def IsProperFace (F X : Set (EuclideanSpace ℝ (Fin d))) : Prop := 
+  F ≠ X ∧ F.Nonempty ∧ IsFace F X
+
+lemma lemma2_27 {F X : Set (EuclideanSpace ℝ (Fin d))} (hXcl : IsClosed X) (hXCV : Convex ℝ X)
+  (hF : IsProperFace F X) : F ⊆ RelativeFrontier X := by
+  rcases hF with ⟨hFX, hF0, hFCV, hFCl, hFs, hFEx⟩
+  unfold RelativeFrontier
+  rcases hF0 with ⟨y, hyF⟩
+  have hyX : y ∈ X := Set.mem_of_subset_of_mem hFs hyF
+  have hFss := (ssubset_of_subset_of_ne hFs hFX) ; clear hFs hFX
+  rcases (Set.nonempty_diff.mpr (HasSSubset.SSubset.not_subset hFss)) with ⟨x, hxX, hxF⟩
+
+  let y'n : ℕ → EuclideanSpace ℝ (Fin d) := λ n => AffineMap.lineMap x y (1 + 1/(n+1):ℝ)
+  let Sn : ℕ → Set (EuclideanSpace ℝ (Fin d)) := λ n => segment ℝ x (y'n n)
+  
+  have h1 : ∀ n, 0 < 1 / ((@Nat.cast ℝ _ n) + 1:ℝ) := 
+    fun n:ℕ => (div_pos zero_lt_one (Nat.cast_add_one_pos n))
+
+  have hySn : ∀ n, y ∈ Sn n := by
+    intro n
+    change y ∈ segment ℝ x (AffineMap.lineMap x y (1 + 1/(n+1):ℝ))
+    rw [segment_eq_image_lineMap]
+    use 1/(1 + 1/(n+1):ℝ)
+    rw [Set.mem_Icc]
+    constructor
+    · -- 1. tedious inequalities
+      sorry
+    · -- 2.
+      rw [AffineMap.coe_lineMap, AffineMap.coe_lineMap]
+      simp only [vsub_eq_sub, vadd_eq_add, add_sub_cancel, ne_eq]
+      rw [one_div, smul_smul, inv_mul_cancel, one_smul, sub_add_cancel]
+      have : 0 < 1 + 1 / (↑n + 1:ℝ) := add_pos_of_nonneg_of_pos (by linarith) (h1 n)
+      exact (ne_of_lt this).symm
+
+  have hy'naff : ∀ n, y'n n ∈ affineSpan ℝ X := by
+    intro n
+    apply affineSpan_mono ℝ
+    rw [Set.insert_subset_iff]
+    exact ⟨hxX, Set.singleton_subset_iff.mpr hyX⟩
+    exact AffineMap.lineMap_mem_affineSpan_pair (1 + 1/(n+1):ℝ) x y
+    done
+
+  have hSnaff : ∀ n, Sn n ⊆ affineSpan ℝ X := by -- By definition 𝑆𝑛 ⊆ aff 𝑋
+    intro n ; clear hXcl hXCV hFCl hFEx hFss hxF hFCV hyF
+    exact Convex.segment_subset (AffineSubspace.convex (affineSpan ℝ X)) 
+      (mem_affineSpan ℝ hxX) (hy'naff n)
+  
+  have hy'nXc : ∀ n, y'n n ∉ X := by
+    intro n hn
+    refine hxF (hFEx hxX hn hyF ?_ ).1 
+    apply mem_openSegment_of_ne_left_right
+    · 
+      rintro rfl
+      exact hxF hyF
+    · 
+      intro hyy'n
+      change (AffineMap.lineMap x y) (1 + 1 / (↑n + 1:ℝ)) = y at hyy'n
+      rw [AffineMap.lineMap_apply x y (1 + 1 / (↑n + 1:ℝ))] at hyy'n
+      have hyy'n1 : (1 + 1 / (↑n + 1:ℝ)) • (y -ᵥ x) +ᵥ x -ᵥ x = y -ᵥ x := by rw [hyy'n]
+      rw [vadd_vsub_assoc, vsub_self, add_zero] at hyy'n1 ; clear hyy'n
+      have hyy'n2 : (1 + 1 / (↑n + 1:ℝ)) • (y -ᵥ x) - (1:ℝ) • (y -ᵥ x) = 0 := 
+        by rw [hyy'n1, one_smul, sub_self]
+      rw [← sub_smul (1 + 1 / (↑n + 1:ℝ)) 1 (y -ᵥ x), add_comm, add_sub_assoc, sub_self, 
+        add_zero, smul_eq_zero] at hyy'n2 ; clear hyy'n1
+      cases' hyy'n2 with h h
+      · 
+        exact (ne_of_lt (h1 n)).symm (by assumption)
+      ·
+        rw [vsub_eq_zero_iff_eq] at h
+        exact hxF (h ▸ hyF)
+    exact hySn n
+    done
+    
+    -- intro _ a ha ; clear hXcl hXCV hFCl hFEx hFss hxF hFCV hyF
+    -- rcases ((Set.mem_image _ _ _).mp ha) with ⟨r, _, rfl⟩ ; clear ha Sn
+    -- apply Set.mem_of_subset_of_mem _ (AffineMap.lineMap_mem_affineSpan_pair r x y)
+    -- apply affineSpan_mono ℝ
+    -- rw [Set.insert_subset_iff]
+    -- exact ⟨hxX, Set.singleton_subset_iff.mpr hyX⟩
+  
+  -- have hy'nXc : ∀ n, y'n n ∉ X := by
+  --   intro n hn 
+  --   have hy'nSn : Sn n = segment ℝ x (y'n n) := by
+  --     rw [segment_eq_image_lineMap] ; clear hXcl hXCV hFCl hFEx hFss hxF hFCV hyF hyX hxX hSnaff
+  --     ext z
+  --     constructor
+  --     · -- 1.
+  --       rintro ⟨r, hr, rfl⟩
+  --       use r / (1 + 1 / (↑n + 1))
+  --       constructor
+  --       · -- 1.
+  --         rw [Set.mem_Icc] ; clear Sn hn y'n x y F X  
+  --         rw [Set.mem_Icc] at hr
+  --         rcases hr with ⟨hr1, hr2⟩
+  --         constructor
+  --         · -- 1.
+  --           have := le_trans hr1 hr2
+  --           rw [div_nonneg_iff]
+  --           left
+  --           exact ⟨ by assumption, by assumption ⟩
+  --         · -- 2.
+  --           rw [div_le_one]
+  --           assumption
+  --           have : 0 < 1/(n + 1 :ℝ) := div_pos zero_lt_one (Nat.cast_add_one_pos n)
+  --           linarith
+  --           done
+  --         done
+  --       · -- 2.
+          
+  --         done
+  --       done
+  --     · -- 2.
+  --       sorry
+  --       done
+  --     -- aesop
+  --     done
+
+
+
+    done
+
+  done 
+
+
+
+
 def Vpolytope {S : Set (EuclideanSpace ℝ (Fin d))} (_ : S.Finite) : 
   Set (EuclideanSpace ℝ (Fin d)) := convexHull ℝ S
 
@@ -103,6 +240,7 @@ lemma ExtremePointsofHpolytope {H_ : Set (Halfspace d)} (hH_ : H_.Finite) (I : E
   · rcases h with ⟨u, hxu, hxu'⟩
     unfold Hpolytope at hxu hxu'
     rw [Set.mem_sInter] at hxu hxu'
+    
     sorry
   · sorry
   done
